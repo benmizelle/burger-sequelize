6ae928e52c80f7c9895ecb9cdce17e6677eb123a
Ben Mizelle <Ben Mizelle>
2017-05-03 21:50:55 -0400

    general save

---
+++ routes/api-routes.js
@@ -0,0 +1,55 @@
+// =====================================================
+// Dependencies
+// =====================================================
+var express = require('express');
+var app = express.Router();
+var db = require('../models');
+
+module.exports = function(app){
+    app.get('/', function(req,res){
+        db.burger.findAll({}).then(function(results){
+            var hbsburger = {
+                burgers: results
+            };
+            // res.json(dbburger);
+            res.render('index', hbsburger);
+        });
+    });
+
+    app.post('/', function(req,res){
+        console.log("req.body" + req.body);
+
+        db.burger.create(req.body).then(function(results){
+            console.log(res.json(results));
+
+        });
+        res.redirect("/");
+    });
+
+    app.delete('/:id', function(req,res){
+        db.burger.destroy({
+            where: {
+                id: req.params.id
+            }
+
+        }).then(function(results){
+            console.log(res.json(results));
+
+        });
+        res.redirect("/");
+    });
+
+    app.put('/:id', function(req,res){
+        db.burger.update(
+            req.body,
+            {
+                where: {
+                    id: req.params.id
+                }
+            }).then(function(results){
+            console.log(res.json(results));
+        });
+        res.redirect("/");
+
+    });
+};
