3218b9c68fa2fccd1ca3906ad2e7321337ffcb8d
Ben Mizelle <Ben Mizelle>
2017-05-03 22:07:14 -0400

    finished setup

--- models/index.js
+++ models/index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// =====================================================
+// Dependencies
+// =====================================================
 var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
